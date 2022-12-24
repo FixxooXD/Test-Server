32d7f7fc3f74537539c2711ded65ee4838460a4f
Fixxoo <rajpalmal40@gmail.com>
2022-12-25 00:42:46 +0530

    Testing-2

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const port =process.env.port || 8080;
 app.get('/', function (req, res) {
   res.sendFile(__dirname,"public/index.html");
 })
-// app.get('/signup',function(req, res){
-//     console.log(req.url)
-//     res.sendFile(path.join(__dirname ,"public/signup.html"))
-// })
+app.get('/signup',function(req, res){
+    console.log(req.url)
+    res.sendFile(path.join(__dirname ,"public/signup.html"))
+})
 
 app.listen(port, (err)=>{
   if(err){
